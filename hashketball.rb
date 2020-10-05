1f1d60d104256d9f3670dc5e498bb1fa41df457f
Rachelle A. Spicer <spicer.rachelle@gmail.com>
2020-10-05 20:08:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -199,9 +199,8 @@ def big_shoe_rebounds
   game_hash.collect do |location, team_data|
     team_data.collect do |attribute, value|
       team_data[:players].each do |player_hash|
-      player_hash[:shoe].each do |biggest_shoe|
-      binding.pry
-    end
+    
+   
     end
   end
 end
